94644ceab3f3b12ae85ba89dfdf1ba9847b3a992
azhamn <azhamnaphiel@gmail.com>
2014-11-29 16:38:17 +0000

    added create user method

--- src/resources/user-actions.php
+++ src/resources/user-actions.php
@@ -1,40 +1,43 @@
 <?php
 
-class Users {
+class Users
+{
 
-	public static function getById($id) {
-		$result = mysql_query('SELECT * FROM `' . DB_PREFIX . 'users` WHERE `id` = \'' . $id . '\' LIMIT 1');
-		if (mysql_num_rows($result) == 0) {
-			return null;
-		} else {
-			return mysql_fetch_array($result, MYSQL_ASSOC);
-		}
-	}
+    public static function getById($id)
+    {
+        $result = mysql_query('SELECT * FROM `' . DB_PREFIX . 'users` WHERE `id` = \'' . $id . '\' LIMIT 1');
+        if (mysql_num_rows($result) == 0) {
+            return null;
+        } else {
+            return mysql_fetch_array($result, MYSQL_ASSOC);
+        }
+    }
 
-	public static function getByCredentials($email, $password) {
-		$result = mysql_query('SELECT * FROM `' . DB_PREFIX . 'users` WHERE `email` = \'' . $email . '\' AND `password` = \'' . Utils::hashPassword($password) . '\' LIMIT 1');
-		if (mysql_num_rows($result) == 0) {
-			return null;
-		} else {
-			return mysql_fetch_array($result, MYSQL_ASSOC);
-		}
-	}
+    public static function getByCredentials($email, $password)
+    {
+        $result = mysql_query('SELECT * FROM `' . DB_PREFIX . 'users` WHERE `email` = \'' . $email . '\' AND `password` = \'' . Utils::hashPassword($password) . '\' LIMIT 1');
+        if (mysql_num_rows($result) == 0) {
+            return null;
+        } else {
+            return mysql_fetch_array($result, MYSQL_ASSOC);
+        }
+    }
 
-	public static function create($name, $email, $pass)
+    public static function create($name, $email, $pass)
 
-		{
-			mysql_query('INSERT INTO '.DB_PREFIX.'
+    {
+        mysql_query('INSERT INTO ' . DB_PREFIX . '
 (email, password, name) VALUES(
 			\'' . $email . '\',
 			\'' . $pass . '\',
 			\'' . $name . '\'
 
  );');
-			$insertId = mysql_insert_id();
-			if ($insertId === false) {
-				return null;
-			} else {
-				return Users::getById($insertId);
-			}
+        $insertId = mysql_insert_id();
+        if ($insertId === false) {
+            return null;
+        } else {
+            return Users::getById($insertId);
+        }
 
-			}
+    }}
