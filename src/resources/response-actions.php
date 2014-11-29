283d3b314a4287224a7e5fc0cf82d8f4755d0226
Mark Ormesher <me@markormesher.co.uk>
2014-11-29 20:34:31 +0000

    Cleaned up response actions file

--- src/resources/response-actions.php
+++ src/resources/response-actions.php
@@ -1,33 +1,24 @@
 <?php
 
-class Responses
-{
-    public static function getById($id)
-    {
-        $result = mysql_query('SELECT * FROM `' . DB_PREFIX . 'response_values` WHERE `response_id` = \'' . $id . '\'');
+class Responses {
 
-        if (mysql_num_rows($result) == 0) {
-
-            return array();
-        } else {
-            $output = array();
-            while ($row = mysql_fetch_array($result, MYSQL_ASSOC))
-            {
-                if ($row['is_text']) {
-                    $result1 = mysql_query('SELECT * FROM `' . DB_PREFIX . 'response_text_values` WHERE `response_text_id` = \'' . $row['value'] . '\'');
-                    echo mysql_error();
-                  $row1=  mysql_fetch_array($result1, MYSQL_ASSOC);
-                    $row['value'] = $row1['value'];
-                }
-
-
-
-                    $output[] = $row;
-
-            }
-            return $output;
-        }
-
-    }
+	public static function getById($id) {
+		$result = mysql_query('SELECT * FROM `' . DB_PREFIX . 'response_values` WHERE `response_id` = \'' . $id . '\';');
+		if (mysql_num_rows($result) == 0) {
+			return array();
+		} else {
+			$output = array();
+			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+				if ($row['is_text']) {
+					$result1 = mysql_query('SELECT * FROM `' . DB_PREFIX . 'response_text_values` WHERE `response_text_id` = \'' . $row['value'] . '\' LIMIT 1;');
+					echo mysql_error();
+					$row1 = mysql_fetch_array($result1, MYSQL_ASSOC);
+					$row['value'] = $row1['value'];
+				}
+				$output[] = $row;
+			}
+			return $output;
+		}
+	}
 
 }
