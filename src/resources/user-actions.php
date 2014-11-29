609b1aad88d169b5fdfb3ad53f2fd9bd95e33c2f
Mark Ormesher <me@markormesher.co.uk>
2014-11-29 15:13:21 +0000

    User get by ID and get by credential

--- src/resources/user-actions.php
+++ src/resources/user-actions.php
@@ -1,14 +1,23 @@
 <?php
 
-class User {
+class Users {
 
 	public static function getById($id) {
-
+		$result = mysql_query('SELECT * FROM `' . DB_PREFIX . 'users` WHERE `id` = \'' . $id . '\' LIMIT 1');
+		if (mysql_num_rows($result) == 0) {
+			return null;
+		} else {
+			return mysql_fetch_array($result, MYSQL_ASSOC);
+		}
 	}
 
 	public static function getByCredentials($email, $password) {
 		$result = mysql_query('SELECT * FROM `' . DB_PREFIX . 'users` WHERE `email` = \'' . $email . '\' AND `password` = \'' . Utils::hashPassword($password) . '\' LIMIT 1');
-
+		if (mysql_num_rows($result) == 0) {
+			return null;
+		} else {
+			return mysql_fetch_array($result, MYSQL_ASSOC);
+		}
 	}
 
 }
