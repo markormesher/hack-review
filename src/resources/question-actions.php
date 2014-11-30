587b23abe1653a6ec267a4f2523f150672adcf40
Mark Ormesher <me@markormesher.co.uk>
2014-11-30 00:24:17 +0000

    Added 'get questions from response id'

--- src/resources/question-actions.php
+++ src/resources/question-actions.php
@@ -15,7 +15,6 @@ class Questions {
 
 	public static function getByEventId($eventId) {
 		$result = mysql_query('SELECT * FROM `' . DB_PREFIX . 'event_questions`, `' . DB_PREFIX . 'questions` WHERE `' . DB_PREFIX . 'event_questions`.`question_id` = `' . DB_PREFIX . 'questions`.`question_id` AND `event_id` = \'' . $eventId . '\' ORDER BY `' . DB_PREFIX . 'event_questions`.`order` ASC;');
-		echo mysql_error();
 		if (mysql_num_rows($result) == 0) {
 			return null;
 		} else {
@@ -23,6 +22,12 @@ class Questions {
 			while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) $output[] = $row;
 			return $output;
 		}
+	}
+
+	public static function getByResponseId($responseId) {
+		$eventId = mysql_query('SELECT * FROM `' . DB_PREFIX . 'responses` WHERE `response_id` = \'' . $responseId . '\' LIMIT 1;');
+		$eventRow = mysql_fetch_array($eventId, MYSQL_ASSOC);
+		return Questions::getByEventId($eventRow['event_id']);
 	}
 
 }
